b0af26d76abe2ad24a481c7f9a904e823c1b7a6e
Aertix <hawk.sj717@gmail.com>
2022-12-20 01:08:01 -0800

    late night

--- js/cursor.js
+++ js/cursor.js
@@ -20,26 +20,27 @@ inputs.forEach(field => {
 });
 
 const thankYou = document.querySelector('.thankYou')
-const form = document.querySelector('.contactForm')
-form.addEventListener('submit', (e) => {
+const formBtn = document.querySelector('#submit')
+formBtn.addEventListener('click', (e) => {
     e.preventDefault()
     console.log('clicked')
     thankYou.classList.add('stayDown')
     Email.send({
         Host: "smtp.elasticemail.com",
         Username: "shawnmail330@gmail.com",
-        Password: "60B4CFB98D3B00C75B91E27171F4B6B07B29",
+        Password: "C980BC664FB05A430E1786DEBFA6FE383B06",
         To: 'shawnmail330@gmail.com',
         From: "shawnmail330@gmail.com",
         Subject: "Web inquiry from: " + document.querySelector('#field2').value,
-        Body: "Hey Shawn, my name is "+ document.querySelector('#field1').value +
-        "<br> Also My Phone number is " + document.querySelector('#field3').value+ 
-        "<br><br>" + document.querySelector('#message').value
-    })
+        Body: "Hey Shawn, my name is " + document.querySelector('#field1').value +
+            "<br> Also My Phone number is " + document.querySelector('#field3').value +
+            "<br><br>" + document.querySelector('#message').value
+    }).then(
+        message => alert(message)
+    );
 })
 
-document.getElementById('submitAgain').addEventListener('click', ()=>{
-    
+document.getElementById('submitAgain').addEventListener('click', () => {
+
     thankYou.classList.remove('stayDown')
-    thankYou.classList.add('goBackUp')
 })
