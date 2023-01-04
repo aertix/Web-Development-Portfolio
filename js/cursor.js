e9ef634d0f7c426d1962b04b02fa53a545e0cc5c
Aertix <hawk.sj717@gmail.com>
2023-01-03 21:59:53 -0800

    link

--- js/cursor.js
+++ js/cursor.js
@@ -31,19 +31,19 @@ formBtn.addEventListener('click', (e) => {
     e.preventDefault()
     console.log('clicked')
     thankYou.classList.add('stayDown')
-    // Email.send({
-    //     Host: "smtp.elasticemail.com",
-    //     Username: "shawnmail330@gmail.com",
-    //     Password: "C980BC664FB05A430E1786DEBFA6FE383B06",
-    //     To: 'shawnmail330@gmail.com',
-    //     From: "shawnmail330@gmail.com",
-    //     Subject: "Web inquiry from: " + document.querySelector('#field2').value,
-    //     Body: "Hey Shawn, my name is " + document.querySelector('#field1').value +
-    //         "<br> Also My Phone number is " + document.querySelector('#field3').value +
-    //         "<br><br>" + document.querySelector('#message').value
-    // }).then(
-    //     message => alert(message)
-    // );
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "shawnmail330@gmail.com",
+        Password: "C980BC664FB05A430E1786DEBFA6FE383B06",
+        To: 'shawnmail330@gmail.com',
+        From: "shawnmail330@gmail.com",
+        Subject: "Web inquiry from: " + document.querySelector('#field2').value,
+        Body: "Hey Shawn, my name is " + document.querySelector('#field1').value +
+            "<br> Also My Phone number is " + document.querySelector('#field3').value +
+            "<br><br>" + document.querySelector('#message').value
+    }).then(
+        message => alert(message)
+    );
 })
 
 document.getElementById('submitAgain').addEventListener('click', () => {
